ed7d02ed2a459176f93e11679b44837758ff1bbf
[HAN-OE5-R] Le Hoang Nam <namlehoang1511@gmail.com>
2017-12-14 16:37:40 +0700

    final commit chapter 4

--- test/controllers/staticpage_controller_test.rb
+++ test/controllers/staticpage_controller_test.rb
@@ -4,25 +4,25 @@ class StaticpageControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get staticpage_home_url
     assert_response :success
-    assert_select "title", "Home"
-  end
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
+end
 
   test "should get help" do
     get staticpage_help_url
     assert_response :success
-    assert_select "title", "Help"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get about" do
     get staticpage_about_url
     assert_response :success
-    assert_select "title", "about"
-  end
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+end
   
   test "should get contact" do
     get staticpage_contact_url
     assert_response :success
-    assert_select "title", "Contact"
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
   
   test "should get root" do
