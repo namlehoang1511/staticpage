3d958bf2aea2f2a9ad2bef061b09d7d27f2d7b51
[HAN-OE5-R] Le Hoang Nam <namlehoang1511@gmail.com>
2017-12-14 08:24:17 +0700

    added staticpage to controller

---
+++ test/controllers/staticpage_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticpageControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get staticpage_home_url
+    assert_response :success
+  end
+
+  test "should get help" do
+    get staticpage_help_url
+    assert_response :success
+  end
+
+end
