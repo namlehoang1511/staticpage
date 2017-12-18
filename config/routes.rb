4d8c5c145ea1f7239e33425f5a553a2418be8810
[HAN-OE5-R] Le Hoang Nam <namlehoang1511@gmail.com>
2017-12-18 17:50:21 +0700

    chapter 6 end?

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   get 'user/new'
-
-  get  '/help',    to: 'staticpage#help', as: 'helf'
-  get  '/about',   to: 'staticpage#about'
-  get  '/contact',  to: 'staticpage#contact'
-  get  '/signup',  to: 'user#new'
-
-
+  get  "/help",    to: "staticpage#help", as: "helf"
+  get  "/about",   to: "staticpage#about"
+  get  "/contact",  to: "staticpage#contact"
+  get  "/signup",  to: "user#new"
+  root "staticpage#home"
   
-  root 'staticpage#home'
-  
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
